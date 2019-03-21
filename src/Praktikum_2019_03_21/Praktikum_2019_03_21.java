1e0709725f78a9782886ed863d1044a51dd8f09b
Bastian Hodapp <bastianhodapp@gmail.com>
2019-03-21 14:43:42 +0100

    Renamed packages and stuff

--- src/Praktikum_2019_03_21/Praktikum_2019_03_21.java
+++ src/Praktikum_2019_03_21/Praktikum_2019_03_21.java
@@ -1,7 +1,9 @@
-public class Main {
+package Praktikum_2019_03_21;
+
+public class Praktikum_2019_03_21 {
 
     public static void main(String[] args) {
-        String stringToAdd = "Baum";
+        String stringToAdd = "Windgrace";
         System.out.println("Hello " + stringToAdd + "!");
     }
 }
