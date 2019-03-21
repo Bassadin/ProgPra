dbcdb6e1ff86ba6529c175398d7170947214f305
Bastian Hodapp <bastianhodapp@gmail.com>
2019-03-21 15:19:59 +0100

    Ignore workspace.xml and tasks.xml

--- src/Praktikum_2019_03_21/Zweierpotenzen.java
+++ src/Praktikum_2019_03_21/Zweierpotenzen.java
@@ -2,9 +2,9 @@ package Praktikum_2019_03_21;
 
 public class Zweierpotenzen {
     public static void main(String[] args) {
-        int base = 9;
+        int base = 11;
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 15; i++) {
             System.out.println(base + " hoch "+ i + " : " + Math.round(Math.pow(base, i)));
         }
     }
