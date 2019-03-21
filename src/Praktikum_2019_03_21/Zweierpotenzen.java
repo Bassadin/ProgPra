05860e7c4dc29392c5893f8458e0ca8dc2180553
Bastian Hodapp <bastianhodapp@gmail.com>
2019-03-21 15:13:29 +0100

    Cleaned empty lines

--- src/Praktikum_2019_03_21/Zweierpotenzen.java
+++ src/Praktikum_2019_03_21/Zweierpotenzen.java
@@ -1,15 +1,11 @@
 package Praktikum_2019_03_21;
 
 public class Zweierpotenzen {
-
     public static void main(String[] args) {
-
         int base = 9;
 
         for (int i = 0; i < 10; i++) {
             System.out.println(base + " hoch "+ i + " : " + Math.round(Math.pow(base, i)));
         }
-
     }
-
 }
