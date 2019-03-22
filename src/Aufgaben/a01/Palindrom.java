f5e2da4fe948332debc663b42b99c3c6d3e51a01
Bastian Hodapp <bastianhodapp@gmail.com>
2019-03-22 13:35:38 +0100

    Finished Palindrom task

--- src/Aufgaben/a01/Palindrom.java
+++ src/Aufgaben/a01/Palindrom.java
@@ -5,27 +5,30 @@ import static prog.ConsoleReader.*;
 public class Palindrom {
 
     public static void main(String[] args) {
-
         String wort = readString();
         palindromTest(wort);
     }
 
     public static void palindromTest(String wort ) {
         if(isPalindrom(wort)) {
-
             System.out.println("Der String " + wort + " ist ein Palindrom");
         }
         else {
-
-            System.out.println("Der String " + wort + " ist ein Palindrom");
+            System.out.println("Der String " + wort + " ist kein Palindrom");
         }
     }
 
-    public static boolean isPalindrom(String wort) {
+    public static boolean isPalindrom(String word) {
+        int wordLength = word.length();
 
-        boolean isPalindrom = false;
-        // Ihr Code
-        return isPalindrom;
+        //Check if word length is odd
+        if ((wordLength & 1) != 0) {
+            return false;
+        }
+
+        String wordFirstHalf = word.toLowerCase().substring(0, (wordLength / 2));
+        String wordSecondHalf = word.toLowerCase().substring((wordLength / 2), wordLength);
+
+        return wordFirstHalf.equals(new StringBuilder(wordSecondHalf).reverse().toString());
     }
 }
-A
