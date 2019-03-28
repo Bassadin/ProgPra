7e599c24240f8c463e3e98f81376668f63649d36
Bastian Hodapp <bastianhodapp@gmail.com>
2019-03-28 10:06:09 +0100

    args in ConsoleTest hinzugefügt

--- src/Praktikum_2019_03_21/ConsoleTest.java
+++ src/Praktikum_2019_03_21/ConsoleTest.java
@@ -1,9 +1,12 @@
 package Praktikum_2019_03_21;
 
 public class ConsoleTest {
-
     public static void main(String[] args) {
-        String stringToAdd = "Windgrace";
-        System.out.println("Hello " + stringToAdd + "!");
+        System.out.print("Hi, ");
+        for (String name :args) {
+            System.out.print(name + " ");
+        }
+
+        System.out.println(". How are you?");
     }
 }
